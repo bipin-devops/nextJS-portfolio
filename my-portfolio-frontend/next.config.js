fb813bed87135db94cec94527d0001cea487d14c
Bipin Raj Sitoula <viraatsitoula@gmail.com>
2023-05-16 10:14:30 +1000

    Added Sass implementation and Typewriter for home page

--- my-portfolio-frontend/next.config.js
+++ my-portfolio-frontend/next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'd33wubrfki0l68.cloudfront.net',
-        port: '',
-        pathname: '**',
+        protocol: "https",
+        hostname: "d33wubrfki0l68.cloudfront.net",
+        port: "",
+        pathname: "**",
       },
     ],
   },
--- my-portfolio-frontend/src/pages/index.js
+++ my-portfolio-frontend/src/pages/index.js
@@ -1,8 +1,8 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-import { Button } from '@mui/material';
-
-import styles from '../styles/Home.module.css';
+import { Button } from "@mui/material";
+import Typewriter from "typewriter-effect";
+import styles from "../styles/Home.module.scss";
 
 export default function Home({ name, summary }) {
   const router = useRouter();
@@ -10,12 +10,38 @@ export default function Home({ name, summary }) {
   return (
     <section className={styles.Home}>
       <h1 className={styles.Name}>{name}</h1>
+      <h1 className={styles.Typewriter}>
+        My Expertise lies in
+        <br />
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(`<span>Javascript</span>`)
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString(`<span>React JS</span>`)
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString(`<span>Node JS</span>`)
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString(`<span>RESTFUL API</span>`)
+              .pauseFor(1000)
+              .deleteAll()
+              .start();
+          }}
+        />
+      </h1>
       <div className={styles.Summary}>{summary}</div>
       <div>
         <Button
           variant="contained"
           size="large"
-          onClick={() => router.push('/projects')}
+          onClick={() => router.push("/projects")}
         >
           Projects
         </Button>
@@ -25,13 +51,13 @@ export default function Home({ name, summary }) {
 }
 
 export async function getStaticProps() {
-  console.log('Running in the server');
+  // console.log('Running in the server');
   // Get your date from an API
   return {
     props: {
-      name: 'Bipin Sitoula',
+      name: "Bipin Sitoula",
       summary:
-        'I am a full-stack developer with proficiency in both front-end and back-end development. I am capable of designing user interfaces, writing code for the client-side, developing server-side logic, and managing databases. My expertise allows me to build functional and responsive websites that meet the needs of clients and users. I have a passion for technology and a desire to continuously learn and improve my skills.',
+        "I am a full-stack developer with proficiency in both front-end and back-end development. I am capable of designing user interfaces, writing code for the client-side, developing server-side logic, and managing databases. My expertise allows me to build functional and responsive websites that meet the needs of clients and users. I have a passion for technology and a desire to continuously learn and improve my skills.",
     },
   };
 }
